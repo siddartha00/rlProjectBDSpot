908d9e3ac6a66dfe30ba13918d6fd657ac119259
Sai Siddartha, Alleni <allenisaisiddharth@gmail.com>
2025-11-23 21:36:05 -0500

    Initial implementation of point extraction

--- door_detect.py
+++ door_detect.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 from pathlib import Path
+import numpy as np
 import os
 
 
@@ -9,6 +10,33 @@ def door_detect(frame):
     model = YOLO(MODEL_PATH)
     result = model.predict(frame, conf=0.80, verbose=False)[0]
     annotated_frame = result.plot()
-    classes = result.boxes.cls.cpu().numpy()
-    boxes = result.boxes.xywh.cpu().numpy()
-    return annotated_frame, classes, boxes
+    classes = result.boxes.cls.cpu().numpy().round().astype(int)
+    boxes = result.boxes.xywh.cpu().numpy().round().astype(int)
+    detections = {'handle': [],
+                  'door': [],
+                  'open_door': []}
+    for class_id, box in zip(classes, boxes):
+        match class_id:
+            case 0:
+                detections['door'].append(box)
+            case 1:
+                detections['handle'].append(box)
+            case 2:
+                detections['open_door'].append(box)
+    return annotated_frame, detections
+
+
+def get_intrinsics(W, H, fovy):
+    cx = W/2
+    cy = H/2
+    fovy = np.deg2rad(fovy)
+    f = H/(2*(np.tan(fovy/2)))
+    k = [[f, 0, cx], [0, f, cy], [0, 0, 1]]
+    return k, cx, cy, f
+
+
+def pixel_2_point(u, v, dz, cx, cy, f):
+    x = (u - cx) * dz / f
+    y = (v - cy) * dz / f
+    z = dz
+    return np.array([x, y, z])
--- sample_env.py
+++ sample_env.py
@@ -3,8 +3,8 @@ import mujoco as mu
 import mujoco.viewer as m
 import time
 import cv2 as cv
-import imutils
-from door_detect import door_detect
+from door_detect import door_detect, get_intrinsics, pixel_2_point
+import numpy as np
 
 # === Paths ===
 cur_path = os.path.abspath(os.path.realpath(__file__))
@@ -16,6 +16,7 @@ try:
     model = mu.MjModel.from_xml_path(env_path)
     data = mu.MjData(model)
     renderer = mu.Renderer(model, height=480, width=640)
+    k, cx, cy, f = get_intrinsics(640, 480, 90)
     print("MuJoCo environment loaded successfully.")
 except Exception as e:
     print(f"Error loading MuJoCo environment: {e}")
@@ -38,14 +39,23 @@ with m.launch_passive(model, data) as viewer:
 
         renderer.update_scene(data, camera="main")  # camera on robot arm
         img = renderer.render()
-        img = imutils.rotate(img, 90)
         renderer.enable_depth_rendering()
         dpth = renderer.render()
-        dpth = imutils.rotate(dpth, 90)
         renderer.disable_depth_rendering()
         dpth_norm = cv.normalize(dpth, None, 0, 255, cv.NORM_MINMAX).astype('uint8')
         img_bgr = cv.cvtColor(img, cv.COLOR_RGB2BGR)   # convert for OpenCV display
-        annotated_frame, classes, boxes = door_detect(img_bgr)
+        annotated_frame, detections = door_detect(img_bgr)
+        if len(detections['handle']) > 0:
+            [u, v] = detections['handle'][0][:2]
+            dz = dpth[v, u]
+            point = pixel_2_point(u, v, dz, cx, cy, f)
+            mu_point = [point[0], -point[1], -point[2]]
+            # print(point)
+            cam_id = mu.mj_name2id(model, mu.mjtObj.mjOBJ_CAMERA, "main")
+            cam_pos = data.cam_xpos[cam_id]
+            cam_rot_matrix = data.cam_xmat[cam_id].reshape(3, 3)
+            point_world = cam_pos + np.dot(cam_rot_matrix, mu_point)
+            print(point_world)
         dpth_disp = cv.applyColorMap(dpth_norm, cv.COLORMAP_PLASMA)
         cv.imshow("Depth View", dpth_disp)
         cv.imshow("Arm Camera View", annotated_frame)
